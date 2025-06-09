46c1e436e452f410cbf84a56c77f8db9acf6ee8e
Fernando <fernandoemiliolopez13@gmail.com>
2025-06-09 01:42:21 -0600

    Tareas añadidas

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,13 @@
 const express = require("express"); //Libreria express importada
 
-const cors = require("cors"); //Libreria express importada
+const cors = require("cors"); //Libreria cors importada
 
 const server = express();
+
+server.use(cors());
+
+server.use(server.json());
+
+server.use("/nodemailer", nodemailer);
 
 module.exports = server;
